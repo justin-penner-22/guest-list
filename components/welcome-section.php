5eb6e2366d80d79f4e2af06fa6358ae55aa5962a
justin-penner-22 <justinpenner80@gmail.com>
2025-03-21 09:30:19 +0100

    Addet pictures. startet menu section. Updated main Slider. Finished welcome section.

--- components/welcome-section.php
+++
@@ -1,9 +0,0 @@
-<div class="container-max">
-    <div class="welcome-container">
-        <div class="welcome-text">
-            <h2>Willkommen im Saruman </h2>
-            <p>Ihrem exklusiven Rückzugsort für luxuriöse, vegetarische Genusserlebnisse.<br> In unserem kleinen, eleganten Ambiente servieren wir kreative und luxuriöse Köstlichkeiten.<br>Unsere mit Liebe und Raffinesse zubereiteten Gerichte vereinen beste Zutaten mit außergewöhnlichen Aromen.<br> Lassen Sie sich von unserer feinen Küche und der persönlichen Atmosphäre verwöhnen.</p>
-        </div>
-        <img alt="staff" src="images/photos/staff.png">
-    </div>
-</div>
