cd6dfc1107a024070ec1b88a8055010dc355eff4
justin-penner-22 <justinpenner80@gmail.com>
2025-04-28 17:57:57 +0200

    Fixed Social Media Icons in phone view

--- pages/static/footer.php
+++ pages/static/footer.php
@@ -27,7 +27,7 @@
     </div>
     <div class="footer-section">
       <h3 class="footer-subheading">Social Media</h3>
-      <ul class="footer-list">
+      <ul class="footer-list-social-media">
         <li class="footer-data"><a href="https://www.facebook.com/" class="footer-icon"><img src="graphics/icons/facebook-logo.svg" class="footer-social-media"></a></li>
         <li class="footer-data"><a href="https://www.youtube.com/" class="footer-icon"><img src="graphics/icons/youtube-logo.svg" class="footer-social-media"></a></li>
         <li class="footer-data"><a href="https://www.instagram.com/" class="footer-icon"><img src="graphics/icons/instagram-logo.svg" class="footer-social-media"></a></li>
