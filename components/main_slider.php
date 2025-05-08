eb903850442cd38b3b5f3b2fc1242b9349fa8e08
justin-penner-22 <justinpenner80@gmail.com>
2025-05-08 14:35:54 +0200

    Added new location page and addet responsiveness

--- components/main_slider.php
+++ components/main_slider.php
@@ -1,7 +1,7 @@
 <header>
         <div class="slides active">
             <picture>
-                <source media="(max-width: 920px)" srcset="images/phone/the-menu-title-phone.webp">
+                <source media="(max-width: 1025px)" srcset="images/phone/the-menu-title-phone.webp">
                 <img class="slider-background-image" src="images/slider/the-menu-title.jpg"></img>
             </picture>
             <a class="prev" onclick="plusSlides(-1)"><img alt="prev" src="graphics/icons/backward-double-arrow.svg"></a>
@@ -10,7 +10,7 @@
 
         <div class="slides">
             <picture>
-                <source media="(max-width: 920px)" srcset="images/phone/chef-title.webp">
+                <source media="(max-width: 1025px)" srcset="images/phone/chef-title.webp">
                 <img class="slider-background-image" src="images/slider/anja-staff.jpg"></img>
             </picture>
             <a class="prev" onclick="plusSlides(-1)"><img alt="prev" src="graphics/icons/backward-double-arrow.svg"></a>
@@ -19,11 +19,10 @@
 
         <div class="slides">
             <picture>
-                <source media="(max-width: 920px)" srcset="images/phone/chef.webp">
+                <source media="(max-width: 1025px)" srcset="images/phone/chef.webp">
                 <img class="slider-background-image" src="images/slider/chef-anja.jpg"></img>
             </picture>
             <a class="prev" onclick="plusSlides(-1)"><img alt="prev" src="graphics/icons/backward-double-arrow.svg"></a>
             <a class="next" onclick="plusSlides(1)"><img alt="next" src="graphics/icons/forward-double-arrow.svg"></a>
-
         </div>
 </header>
--- pages/location.php
+++ pages/location.php
@@ -1,8 +1,37 @@
 
-<div class="location-seperator"><h2 class="location-heading">SO FINDEST DU UNS!</h2></div>
 <div class="container-max">
   <div class="container">
-    <div style="width: 100%"><iframe width="100%" height="800" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Dorfstra%C3%9Fe%208%20%20%2016818%20Pfalzheim+(Saruman)&amp;t=h&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/collections/drones/">drone quadcopter</a></iframe></div>
+    <div class="location-container">
+      <div class="location-map">
+        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2323.5146044388844!2d13.69564301329786!3d54.38322497249382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ab3e737923f2f5%3A0x2a766e8915125062!2sLift%20Sellin%20Pier!5e0!3m2!1sen!2sde!4v1746704853126!5m2!1sen!2sde" width="100%" height="600" style="border-radius: 10px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+      </div> 
+        <div class="location-time">
+        <div>
+          <h2>Hawthorn Blue Lagoon</h2>
+          <h3>Öffnungszeiten</h3>
+          <p>Montag: Geschlossen<br>
+            Dienstag: 17:00 – 22:00 Uhr<br>
+            Mittwoch: 17:00 – 22:00 Uhr<br>
+            Donnerstag: 17:00 – 22:00 Uhr<br>
+            Freitag: 17:00 – 23:00 Uhr<br>
+            Samstag: 12:00 – 23:00 Uhr<br>
+            Sonntag: 12:00 – 21:00 Uhr
+          </p>
+        </div>
+        <img src="images/others/location1.jpg" alt="location" class="location-image1">
+      </div>
+    </div>
   </div>
 </div>
-<div class="location-seperator"></div>
+
+<div class="container-max">
+  <div class="container">
+    <div class="location-box">
+      <p class="location-text"><span class="bold">Das exklusive Restaurant Heathrow</span><br> liegt abgelegen auf einer malerischen Insel, die nur per Boot erreichbar ist. Eingebettet in unberührte Natur bietet der Standort eine atemberaubende Aussicht auf das weite, glitzernde Meer. Die salzige Meeresbrise und das sanfte Rauschen der Wellen verleihen dem kulinarischen Erlebnis eine besondere Tiefe. Wer hier speist, genießt nicht nur außergewöhnliche Gerichte, sondern auch die Ruhe und Schönheit einer fast surrealen Umgebung.</p>
+      <img src="images/others/location2.jpeg" alt="location" class="location-image2">
+    </div>
+  </div>
+</div>
+
+
+
